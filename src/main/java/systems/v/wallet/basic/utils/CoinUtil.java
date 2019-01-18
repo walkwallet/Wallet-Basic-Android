06ea9ffc2f6efb7fddec01b638d849f0a04f5727
walktea <walkwallet@yahoo.com>
2019-01-18 15:49:57 +0800

    fix: the decimal scale validation of amount

--- src/main/java/systems/v/wallet/basic/utils/CoinUtil.java
+++ src/main/java/systems/v/wallet/basic/utils/CoinUtil.java
@@ -6,7 +6,8 @@ import java.math.BigDecimal;
 
 public class CoinUtil {
 
-    private static final String UNIT = " VSYS";
+    private static final String UNIT = "VSYS";
+    private static final BigDecimal MAX_LONG = new BigDecimal(Long.MAX_VALUE).movePointLeft(8);
 
     public static String format(long amount) {
         BigDecimal decimal = new BigDecimal(amount).movePointLeft(8);
@@ -17,29 +18,26 @@ public class CoinUtil {
     }
 
     public static String formatWithUnit(long amount) {
-        return format(amount) + UNIT;
+        return format(amount) + " " + UNIT;
     }
 
     public static long parse(String amount) {
         if (TextUtils.isEmpty(amount)) {
             return 0;
         }
-        amount = amount.replace(UNIT, "");
+        amount = amount.replace(UNIT, "").trim();
         if (!validate(amount)) {
             return 0;
         }
         BigDecimal decimal = new BigDecimal(amount).movePointRight(8);
-        BigDecimal maxLong = new BigDecimal(Long.MAX_VALUE);
-        if (decimal.compareTo(maxLong) > 0) {
-            return 0;
-        }
         return decimal.longValueExact();
     }
 
     public static boolean validate(String amount) {
         try {
             BigDecimal decimal = new BigDecimal(amount);
-            return decimal.compareTo(BigDecimal.ZERO) >= 0 && decimal.scale() <= 6;
+            return decimal.compareTo(MAX_LONG) <= 0 &&
+                    decimal.compareTo(BigDecimal.ZERO) >= 0 && decimal.scale() <= 8;
         } catch (Exception e) {
             return false;
         }
